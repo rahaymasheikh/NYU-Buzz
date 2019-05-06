481db70d96628a67e356b64ed60a26503d014c1b
Max McHarris <am7155@nyu.edu>
2019-05-05 22:10:12 -0400

    Completed (untested) integratable code for HomeViewController

--- HomeViewController.swift
+++ HomeViewController.swift
@@ -5,7 +5,6 @@
 //  Created by nyuguest on 5/5/19.
 //  Copyright © 2019 Team RSM. All rights reserved.
 //
-
 import UIKit
 import CoreLocation
 
@@ -36,16 +35,46 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate {
     
     // checks if user is at an event they subscribed for, if so, rewards user! (called after we get user location)
     func checkIfAtEvent(user: User, atLocation: CLLocation, onDate: Date) {
-        var matchingEvent: Event
+        var matchingEvent: Event?
+        var matchingEventInd: Int?
         for (index, event) in user.events.enumerated() {
             if (atLocation.distance(from: event.location) <= addressDist) {
                 if (event.startDate <= onDate && event.endDate <= event.endDate) {
                     matchingEvent = event
+                    matchingEventInd = index
                     break;
                 }
             }
         }
-        // TODO: check if matchingEvent isn't null,  if so, do all of that!
+        // TODO: check if matchingEvent isn't null,  if so, play alert, update tokens, delete event
+        if (matchingEvent != nil) {
+            let alert = UIAlertController(title: "Checked in!", message: "You have successfuly checked in for the event \(matchingEvent!.name).", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                NSLog("The \"OK\" alert occured.")
+            }))
+            self.present(alert, animated: true, completion: nil)
+            addTokens(forUser: user, tokens: matchingEvent!.tokens)
+            deleteEvent(atIndex: matchingEventInd!, forUser: user)
+        }
+        else {
+            let alert = UIAlertController(title: "No events", message: "You are not scheduled for any events at this time or location", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                NSLog("The \"OK\" alert occured.")
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    // TODO: updates value and display of tokens
+    func addTokens(forUser: User, tokens: Int) {
+        user.tokens += tokens;
+        // TODO: update display of tokens
+    }
+    
+    // TODO: deletes user event and event in display
+    func deleteEvent(atIndex: Int, forUser: User) {
+        user.events.remove(at: atIndex)
+        // TODO: update the table display
     }
     
     override func didReceiveMemoryWarning() {
